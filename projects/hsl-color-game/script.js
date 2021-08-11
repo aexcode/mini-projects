6cc206fda9df2a4a9ba16286ca49a981faf62e2b
Ayla Ex <aylaex@protonmail.com>
2021-08-11 12:32:55 -0400

    add navigation

--- projects/hsl-color-game/script.js
+++ projects/hsl-color-game/script.js
@@ -1,16 +1,29 @@
 // cached elements ==========
 const navbar = document.getElementById('navbar')
-const navLinks = [...navbar.children]
-const sections = [...document.getElementsByTagName('section')]
+const navigation = [...document.getElementsByClassName('nav-link')].map(
+  (link) =>
+    (link = {
+      link: link,
+      section: document.getElementById(link.hash.slice(1)),
+    }),
+)
 
 // event listeners ==========
-navLinks.forEach((link) => link.addEventListener('click', handleNavigation))
+navigation.forEach((item) =>
+  item.link.addEventListener('click', handleNavigation),
+)
 
 // event handlers ==========
 function handleNavigation(e) {
-  sections.forEach((section) => {
-    if (section.id === e.target.innerText.toLowerCase()) {
-      return section.classList.remove('display-none')
-    } else return section.classList.add('display-none')
+  navigation.forEach((item) => {
+    if (item.section.id === e.target.hash.slice(1)) {
+      item.link.classList.add('nav-link-active')
+      item.section.classList.remove('display-none')
+      return
+    } else {
+      item.link.classList.remove('nav-link-active')
+      item.section.classList.add('display-none')
+      return
+    }
   })
 }
