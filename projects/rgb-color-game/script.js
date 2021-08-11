75d20f06baedc5173be942d6ead83d19e174884b
Ayla Ex <aylaex@protonmail.com>
2021-08-11 14:04:19 -0400

    clean up code

--- projects/rgb-color-game/script.js
+++ projects/rgb-color-game/script.js
@@ -7,6 +7,9 @@ const navigation = [...document.getElementsByClassName('nav-link')].map(
       section: document.getElementById(link.hash.slice(1)),
     }),
 )
+const rgb = document.getElementById('rgb')
+const colors = [...document.getElementsByClassName('color')]
+const fillColors = []
 
 // event listeners ==========
 navigation.forEach((item) =>
@@ -19,6 +22,7 @@ function handleNavigation(e) {
     if (item.section.id === e.target.hash.slice(1)) {
       item.link.classList.add('nav-link-active')
       item.section.classList.remove('display-none')
+      if (item.section.id === 'game') generateColors()
       return
     } else {
       item.link.classList.remove('nav-link-active')
@@ -27,3 +31,26 @@ function handleNavigation(e) {
     }
   })
 }
+
+// helper functions ==========
+function generateColor() {
+  const randomVal = () => Math.floor(Math.random() * 255)
+  return `rgb(${randomVal()}, ${randomVal()}, ${randomVal()})`
+}
+
+function generateFillColors() {
+  fillColors.length = 0
+  colors.forEach((color) => fillColors.push(generateColor()))
+}
+
+function displayColors() {
+  rgb.innerText = fillColors[Math.floor(Math.random() * fillColors.length)]
+
+  colors.forEach((color, i) => {
+    color.style.backgroundColor = fillColors[i]
+  })
+}
+
+// on load ==========
+generateFillColors()
+displayColors()
